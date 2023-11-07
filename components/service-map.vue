2707b90631bde4244ecea8476990e0b36f99b629
jay-p-b-7span <jay.p.b@7span.com>
2023-11-07 09:49:21 +0530

    updates

--- components/service-map.vue
+++ components/service-map.vue
@@ -5,77 +5,22 @@
         <h3
             class="mb-2 text-2xl md:text-4xl text-primary-500 font-bold tracking-tight dark:text-white"
         >
-            Services Provided
+            Project Locations
         </h3>
         <p class="font-normal text-gray-700 dark:text-gray-400">
-            Completed 10+ Projects at various places
+            Completed 15+ Projects in Different states of India
         </p>
 
         <!-- <div class="relative"> -->
-        <MapboxMap
-            style="position: relative !important"
-            class="h-[500px] mt-4"
-            map-id="292939"
-            :options="{
-                style: 'mapbox://styles/mapbox/streets-v12',
-                zoom: getZoom(), // starting zoom
-                center: [
-                    $options.location.indore.longitude,
-                    $options.location.indore.latitude,
-                ], // starting position
-            }"
-        >
-            <MapboxDefaultMarker
-                marker-id="292939"
-                :options="{}"
-                :lnglat="[
-                    $options.location.rajkot.longitude,
-                    $options.location.rajkot.latitude,
-                ]"
-            />
 
-            <MapboxDefaultMarker
-                marker-id="292937"
-                :options="{}"
-                :lnglat="[
-                    $options.location.ahmedabad.longitude,
-                    $options.location.ahmedabad.latitude,
-                ]"
-            />
+        <img src="/images/service-location.png" class="mx-auto mt-4" />
 
-            <MapboxDefaultMarker
-                marker-id="292937"
-                :options="{}"
-                :lnglat="[
-                    $options.location.mumbai.longitude,
-                    $options.location.mumbai.latitude,
-                ]"
-            />
-        </MapboxMap>
         <!-- </div> -->
     </div>
 </template>
 
 <script>
 export default {
-    location: {
-        indore: {
-            latitude: 22.719568,
-            longitude: 75.857727,
-        },
-        mumbai: {
-            latitude: 19.155001,
-            longitude: 72.849998,
-        },
-        rajkot: {
-            latitude: 22.308155,
-            longitude: 70.800705,
-        },
-        ahmedabad: {
-            latitude: 23.033863,
-            longitude: 72.585022,
-        },
-    },
     methods: {
         getZoom() {
             if (process.client) {
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "nuxt-mapbox"],
-    mapbox: {
-        accessToken:
-            "pk.eyJ1IjoiZGF2aWRyYW1pMTIiLCJhIjoiY2xlcmowN25oMGM4YjN4cWo1d2VjdWtlbiJ9._i-alLPw_HV5Oaln2pNYtQ",
-    },
+    modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
 });
