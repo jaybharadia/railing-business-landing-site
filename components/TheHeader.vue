120719c0a16f623debf9c99b47a5a39cebec2fe0
jay-p-b-7span <jay.p.b@7span.com>
2023-11-11 20:25:37 +0530

    Updates for footer

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div
-            class="relative max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+            class="relative max-w-screen-xl flex flex-nowrap items-center justify-between mx-auto p-4"
         >
             <a href="https://flowbite.com/" class="flex items-center">
                 <img
---
+++ utils/common.js
@@ -0,0 +1,19 @@
+export const getCompanyName = () => {
+    return "Decent Steel";
+};
+
+export const getCurrentYear = () => {
+    return new Date().getFullYear();
+};
+
+export const getPhoneNumber = () => {
+    return "9714845791";
+};
+
+export const getWhatsappNumber = () => {
+    return "918849116635";
+};
+
+export const getEmail = () => {
+    return "decentsteelrails@gmail.com";
+};
